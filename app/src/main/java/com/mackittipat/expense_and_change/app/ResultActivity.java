58a23674104605e9935dcdbf6044a9c10355a0dc
Mac <mac.kittipat@gmail.com>
2014-05-01 18:16:02 +0700

    Implement ExpenseAndChange in ResultActivity

--- app/src/main/java/com/mackittipat/expense_and_change/app/ResultActivity.java
+++ app/src/main/java/com/mackittipat/expense_and_change/app/ResultActivity.java
@@ -5,9 +5,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ListView;
 import com.mackittipat.expense_and_change.app.listview.adapter.ChangeListViewAdapter;
+import com.mackittipat.expense_and_change.core.ExpenseAndChange;
 import com.mackittipat.expense_and_change.core.model.ChangeModel;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class ResultActivity extends Activity {
@@ -19,11 +19,9 @@ public class ResultActivity extends Activity {
 
         Intent intent = getIntent();
         long expense = intent.getLongExtra(MainActivity.EXPENSE, 0);
-        System.out.println("Expense = " + expense);
 
-        List<ChangeModel> changeModelList = new ArrayList<ChangeModel>();
-        changeModelList.add(new ChangeModel(1, 2));
-        changeModelList.add(new ChangeModel(3, 4));
+        ExpenseAndChange expenseAndChange = new ExpenseAndChange();
+        List<ChangeModel> changeModelList = expenseAndChange.calculate(expense);
 
         ChangeListViewAdapter changeListViewAdapter = new ChangeListViewAdapter(this, changeModelList);
         ListView listViewChange = (ListView) findViewById(R.id.lview_result);
