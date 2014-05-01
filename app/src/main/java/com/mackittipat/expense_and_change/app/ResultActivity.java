80f0b90bdd402ab4d82226b260f3e3e2433ad582
Mac <mac.kittipat@gmail.com>
2014-05-01 15:37:08 +0700

    Create ResultActivity

---
+++ app/src/main/java/com/mackittipat/expense_and_change/app/ResultActivity.java
@@ -0,0 +1,13 @@
+package com.mackittipat.expense_and_change.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ResultActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.result);
+    }
+}
