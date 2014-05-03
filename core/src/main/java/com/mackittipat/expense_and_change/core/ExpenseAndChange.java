c5ae035d55e1bf9bd9876a96d8908e3db438d6f2
Mac <mac.kittipat@gmail.com>
2014-05-03 12:04:22 +0700

    Add logback to core

--- core/src/main/java/com/mackittipat/expense_and_change/core/ExpenseAndChange.java
+++ core/src/main/java/com/mackittipat/expense_and_change/core/ExpenseAndChange.java
@@ -1,24 +1,31 @@
 package com.mackittipat.expense_and_change.core;
 
 import com.mackittipat.expense_and_change.core.model.ChangeModel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ExpenseAndChange {
 
+    public static final Logger log = LoggerFactory.getLogger(ExpenseAndChange.class);
+
     long[] thaiMonies = new long[] {1000, 500, 100, 50, 40, 20, 10, 5, 1};
 
     public List<ChangeModel> calculate(long expense) {
 
         List<ChangeModel> result = new ArrayList<ChangeModel>();
-
+        log.debug("Calculating expense and change, expense = {}", expense);
         for(long thaiMoney : thaiMonies) {
+            log.debug("possible expense = {}", thaiMoney);
             if(thaiMoney > expense) {
                 ChangeModel changeModel = new ChangeModel();
                 changeModel.setPossibleExpense(thaiMoney);
                 changeModel.setChange(thaiMoney - expense);
                 result.add(changeModel);
+                log.debug("possible expense ({}) > expense ({}), change = {}",
+                        changeModel.getPossibleExpense(), expense, changeModel.getChange());
             }
         }
 
